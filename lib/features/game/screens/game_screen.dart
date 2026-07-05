f10a8dfd006782c84bf26598d044ee7ed04e27d4
cbnemrhn293 <emirhanfb12377@gmail.com>
2026-07-05 16:06:11 +0300

    fix: import flutter_animate in game_screen.dart to resolve compilation errors

--- lib/features/game/screens/game_screen.dart
+++ lib/features/game/screens/game_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:math';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:go_router/go_router.dart';
